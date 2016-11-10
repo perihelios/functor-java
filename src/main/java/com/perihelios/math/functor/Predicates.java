dc5d88240949e234e80d7f4473a59922b6634606
Jonathan Cottrill <github@jonathancottrill.net>
2016-11-10 12:40:08 -0600

    Add palindrome predicates

--- src/main/java/com/perihelios/math/functor/Predicates.java
+++ src/main/java/com/perihelios/math/functor/Predicates.java
@@ -18,4 +18,40 @@ public class Predicates {
 	public static boolean odd(long n) {
 		return (n & 1L) == 1L;
 	}
+
+	public static boolean palindrome(BigInteger n) {
+		return palindrome(n.toString().toCharArray());
+	}
+
+	public static boolean palindrome(long n) {
+		return palindrome(Long.toString(n).toCharArray());
+	}
+
+	public static boolean palindrome(String str) {
+		return palindrome(str.toCharArray());
+	}
+
+	public static boolean palindrome(char[] chars) {
+		int length = chars.length;
+
+		if (length < 2) {
+			return true;
+		}
+
+		int remainder = length & 1;
+		int right = length >>> 1;
+		int left = right - 1;
+		right += remainder;
+
+		while (left >= 0) {
+			if (chars[left] != chars[right]) {
+				return false;
+			}
+
+			left--;
+			right++;
+		}
+
+		return true;
+	}
 }
--- src/test/java/com/perihelios/math/functor/PredicatesTest.java
+++ src/test/java/com/perihelios/math/functor/PredicatesTest.java
@@ -2,8 +2,11 @@ package com.perihelios.math.functor;
 
 import org.junit.Test;
 
+import java.math.BigInteger;
+
 import static com.perihelios.math.functor.Predicates.even;
 import static com.perihelios.math.functor.Predicates.odd;
+import static com.perihelios.math.functor.Predicates.palindrome;
 import static java.math.BigInteger.valueOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -43,5 +46,34 @@ public class PredicatesTest {
 		assertThat(odd(-1L), is(true));
 		assertThat(odd(2L), is(false));
 		assertThat(odd(-2L), is(false));
+	}
+
+	@Test
+	public void palindrome_BigInteger() {
+		assertThat(palindrome(new BigInteger("11111122222322222111111")), is(true));
+		assertThat(palindrome(new BigInteger("11111122222332222111111")), is(false));
+	}
+
+	@Test
+	public void palindrome_long() {
+		assertThat(palindrome(1111122223222211111L), is(true));
+		assertThat(palindrome(1111122223322211111L), is(false));
+	}
+
+	@Test
+	public void palindrome_String() {
+		assertThat(palindrome("aba"), is(true));
+		assertThat(palindrome("abb"), is(false));
+	}
+
+	@Test
+	public void palindrome_char_array() {
+		assertThat(palindrome("".toCharArray()), is(true));
+		assertThat(palindrome("a".toCharArray()), is(true));
+		assertThat(palindrome("aa".toCharArray()), is(true));
+		assertThat(palindrome("ab".toCharArray()), is(false));
+		assertThat(palindrome("aba".toCharArray()), is(true));
+		assertThat(palindrome("abb".toCharArray()), is(false));
+		assertThat(palindrome("abba".toCharArray()), is(true));
 	}
 }
