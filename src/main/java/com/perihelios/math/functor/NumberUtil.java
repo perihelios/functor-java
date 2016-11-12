8753a9c0e9d99fb97b0ce231e8dff54ba09a2fcb
Jonathan Cottrill <github@jonathancottrill.net>
2016-11-11 21:18:59 -0600

    Add gridFromString parsing utility

--- src/main/java/com/perihelios/math/functor/NumberUtil.java
+++ src/main/java/com/perihelios/math/functor/NumberUtil.java
@@ -17,6 +17,55 @@ public class NumberUtil {
 		return bigIntegers;
 	}
 
+	public static BigInteger[][] gridFromString(String grid) {
+		grid = grid.trim();
+
+		if (grid.isEmpty()) {
+			return new BigInteger[0][];
+		}
+
+		String[] rows = grid.split("\r\n|\n");
+		int rowCount = rows.length;
+		int columnCount = -1;
+
+		BigInteger[][] arr = new BigInteger[rowCount][];
+
+		for (int r = 0; r < rowCount; r++) {
+			String row = rows[r].trim();
+
+			String[] columns;
+			if (row.length() == 0) {
+				columns = new String[0];
+			} else {
+				columns = row.split("\\s+");
+			}
+
+			if (columnCount < 0) {
+				columnCount = columns.length;
+			} else {
+				if (columns.length != columnCount) {
+					throw new IllegalArgumentException("Row " + r + " has " + columns.length + " column(s); expected " + columnCount);
+				}
+			}
+
+			arr[r] = new BigInteger[columnCount];
+
+			for (int c = 0; c < columnCount; c++) {
+				BigInteger value;
+
+				try {
+					value = new BigInteger(columns[c]);
+				} catch (NumberFormatException e) {
+					throw new IllegalArgumentException("Value at " + r + ", " + c + " not a decimal integer: " + columns[c]);
+				}
+
+				arr[r][c] = value;
+			}
+		}
+
+		return arr;
+	}
+
 	// Prevent instantiation of this class
 	private NumberUtil() {}
 }
--- src/test/java/com/perihelios/math/functor/NumberUtilTest.java
+++ src/test/java/com/perihelios/math/functor/NumberUtilTest.java
@@ -6,9 +6,11 @@ import java.math.BigInteger;
 
 import static com.perihelios.math.functor.NumberUtil.bigInt;
 import static com.perihelios.math.functor.NumberUtil.bigInts;
+import static com.perihelios.math.functor.NumberUtil.gridFromString;
 import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class NumberUtilTest {
 	@Test
@@ -21,5 +23,71 @@ public class NumberUtilTest {
 		assertThat(bigInts().length, is(0));
 		assertThat(bigInts(1), is(arrayContaining(BigInteger.valueOf(1L))));
 		assertThat(bigInts(1, 2), is(arrayContaining(BigInteger.valueOf(1L), BigInteger.valueOf(2L))));
+	}
+
+	@Test
+	public void gridFromString_works() {
+		assertThat(gridFromString("").length, is(0));
+
+		assertThat(gridFromString("1"), is(arrayContaining(new BigInteger[][]{
+			bigInts(1),
+		})));
+
+		assertThat(gridFromString("1 2"), is(arrayContaining(new BigInteger[][]{
+			bigInts(1, 2),
+		})));
+
+		assertThat(gridFromString("1\t2"), is(arrayContaining(new BigInteger[][]{
+			bigInts(1, 2),
+		})));
+
+		assertThat(gridFromString("   1  \t  2 \t"), is(arrayContaining(new BigInteger[][]{
+			bigInts(1, 2),
+		})));
+
+		assertThat(gridFromString("1 2\n3 4\r\n5 6"), is(arrayContaining(new BigInteger[][]{
+			bigInts(1, 2),
+			bigInts(3, 4),
+			bigInts(5, 6),
+		})));
+
+		assertThat(gridFromString("1 2\n3 4\n"), is(arrayContaining(new BigInteger[][]{
+			bigInts(1, 2),
+			bigInts(3, 4),
+		})));
+	}
+
+	@Test
+	public void gridFromString_expects_rows_to_have_consistent_columns() {
+		try {
+			gridFromString("1\n\n2");
+			fail("Expected exception " + IllegalArgumentException.class.getName());
+		} catch (IllegalArgumentException expected) {
+			assertThat(expected.getMessage(), is("Row 1 has 0 column(s); expected 1"));
+		}
+
+		try {
+			gridFromString("1 2\n3");
+			fail("Expected exception " + IllegalArgumentException.class.getName());
+		} catch (IllegalArgumentException expected) {
+			assertThat(expected.getMessage(), is("Row 1 has 1 column(s); expected 2"));
+		}
+	}
+
+	@Test
+	public void gridFromString_expects_decimal_integers() {
+		try {
+			gridFromString("a");
+			fail("Expected exception " + IllegalArgumentException.class.getName());
+		} catch (IllegalArgumentException expected) {
+			assertThat(expected.getMessage(), is("Value at 0, 0 not a decimal integer: a"));
+		}
+
+		try {
+			gridFromString("1 2\n3 4.1");
+			fail("Expected exception " + IllegalArgumentException.class.getName());
+		} catch (IllegalArgumentException expected) {
+			assertThat(expected.getMessage(), is("Value at 1, 1 not a decimal integer: 4.1"));
+		}
 	}
 }
