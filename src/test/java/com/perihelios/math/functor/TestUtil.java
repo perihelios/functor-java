ba46114c41218ad724833231a9c4351164a50172
Jonathan Cottrill <github@jonathancottrill.net>
2016-11-14 12:12:15 -0600

    Add treeSet test helper

--- src/test/java/com/perihelios/math/functor/TestUtil.java
+++ src/test/java/com/perihelios/math/functor/TestUtil.java
@@ -2,11 +2,13 @@ package com.perihelios.math.functor;
 
 import java.lang.reflect.Field;
 import java.util.SortedMap;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class TestUtil {
 	public static <K, V> SortedMap<K, V> treeMap(K key, V value, Object... others) {
-		TreeMap<K, V> map = new TreeMap<>();
+		SortedMap<K, V> map = new TreeMap<>();
 		map.put(key, value);
 
 		int length = others.length;
@@ -26,6 +28,16 @@ public class TestUtil {
 		}
 
 		return map;
+	}
+
+	public static <T> SortedSet<T> treeSet(T... values) {
+		SortedSet<T> set = new TreeSet<>();
+
+		for (T value : values) {
+			set.add(value);
+		}
+
+		return set;
 	}
 
 	public static Object getField(Object instance, String fieldName) {
