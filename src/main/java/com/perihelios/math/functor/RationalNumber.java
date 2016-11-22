8d9610cca173e1d7418b227fa0d869186f1dbfb7
Jonathan Cottrill <github@jonathancottrill.net>
2016-11-21 23:28:40 -0600

    Add RationalNumber (incomplete implementation)

---
+++ src/main/java/com/perihelios/math/functor/RationalNumber.java
@@ -0,0 +1,193 @@
+package com.perihelios.math.functor;
+
+public class RationalNumber extends Number implements Comparable<RationalNumber> {
+	private static final PrimeEngine ENGINE = new SimplePrimeEngine(new BinarySearchSquareRootEngine());
+
+	private final long numerator;
+	private final long denominator;
+	private final boolean negative;
+
+	public RationalNumber(long numerator, long denominator) {
+		if (numerator == 0L) {
+			this.numerator = 0L;
+			this.denominator = 1L;
+			this.negative = false;
+
+			return;
+		}
+
+		if (numerator < 0L) {
+			numerator = -numerator;
+
+			if (denominator < 0L) {
+				denominator = -denominator;
+				negative = false;
+			} else {
+				negative = true;
+			}
+		} else {
+			if (denominator < 0L) {
+				denominator = -denominator;
+				negative = true;
+			} else {
+				negative = false;
+			}
+		}
+
+		if (numerator == 1L) {
+			this.numerator = 1L;
+			this.denominator = denominator;
+
+			return;
+		}
+
+		if (denominator == 1L) {
+			this.numerator = numerator;
+			this.denominator = 1L;
+
+			return;
+		}
+
+		LowestCommonMultipleBucket bucket = new LowestCommonMultipleBucket(ENGINE);
+		bucket.add(numerator);
+		bucket.add(denominator);
+
+		long lcm = bucket.lcm();
+
+		this.numerator = lcm / denominator;
+		this.denominator = lcm / numerator;
+	}
+
+	public long numerator() {
+		return numerator;
+	}
+
+	public long denominator() {
+		return denominator;
+	}
+
+	public RationalNumber multiply(long n) {
+		throw new UnsupportedOperationException("Method not implemented");
+	}
+
+	public RationalNumber multiply(RationalNumber n) {
+		throw new UnsupportedOperationException("Method not implemented");
+	}
+
+	public RationalNumber add(long n) {
+		long signedNumerator = negative ? -numerator : numerator;
+
+		return new RationalNumber(signedNumerator + n * denominator, denominator);
+	}
+
+	public RationalNumber add(RationalNumber n) {
+		long signedNumerator = negative ? -numerator : numerator;
+		long signedNumeratorN = n.negative ? -n.numerator : n.numerator;
+		long denominator = this.denominator;
+		long denominatorN = n.denominator;
+
+		if (denominator == 1L) {
+			return n.add(signedNumerator);
+		}
+
+		if (denominatorN == 1L) {
+			return add(signedNumeratorN);
+		}
+
+		LowestCommonMultipleBucket bucket = new LowestCommonMultipleBucket(ENGINE);
+		bucket.add(denominator);
+		bucket.add(denominatorN);
+
+		long lcm = bucket.lcm();
+
+		return new RationalNumber(signedNumerator * (lcm / denominator) + signedNumeratorN * (lcm / denominatorN), lcm);
+	}
+
+	public RationalNumber subtract(long n) {
+		throw new UnsupportedOperationException("Method not implemented");
+	}
+
+	public RationalNumber subtract(RationalNumber n) {
+		throw new UnsupportedOperationException("Method not implemented");
+	}
+
+	public boolean isInteger() {
+		return numerator == 0L || denominator == 1L;
+	}
+
+	public boolean isNegative() {
+		return negative;
+	}
+
+	public RationalNumber reciprocal() {
+		if (negative) {
+			return new RationalNumber(-denominator, numerator);
+		} else {
+			return new RationalNumber(denominator, numerator);
+		}
+	}
+
+	public long signum() {
+		if (negative) {
+			return -1L;
+		} else {
+			return 1L;
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		RationalNumber that = (RationalNumber) o;
+
+		return negative == that.negative && numerator == that.numerator && denominator == that.denominator;
+	}
+
+	@Override
+	public int hashCode() {
+		return (negative ? 1 : 0) + 7 * Long.hashCode(numerator) + 31 * Long.hashCode(denominator);
+	}
+
+	@Override
+	public int compareTo(RationalNumber o) {
+		throw new UnsupportedOperationException("Method not implemented");
+	}
+
+	@Override
+	public int intValue() {
+		if (negative) {
+			return (int) (-numerator / denominator);
+		} else {
+			return (int) (numerator / denominator);
+		}
+	}
+
+	@Override
+	public long longValue() {
+		if (negative) {
+			return -numerator / denominator;
+		} else {
+			return numerator / denominator;
+		}
+	}
+
+	@Override
+	public float floatValue() {
+		if (negative) {
+			return -numerator / (float) denominator;
+		} else {
+			return numerator / (float) denominator;
+		}
+	}
+
+	@Override
+	public double doubleValue() {
+		if (negative) {
+			return -numerator / (double) denominator;
+		} else {
+			return numerator / (double) denominator;
+		}
+	}
+}
---
+++ src/test/java/com/perihelios/math/functor/RationalNumberTest.java
@@ -0,0 +1,217 @@
+package com.perihelios.math.functor;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class RationalNumberTest {
+	@Test
+	public void constructor_reduces() {
+		RationalNumber r;
+
+		r = new RationalNumber(2, 2);
+		assertThat(r.numerator(), is(1L));
+		assertThat(r.denominator(), is(1L));
+
+		r = new RationalNumber(4, 6);
+		assertThat(r.numerator(), is(2L));
+		assertThat(r.denominator(), is(3L));
+
+		r = new RationalNumber(10000, 2000);
+		assertThat(r.numerator(), is(5L));
+		assertThat(r.denominator(), is(1L));
+	}
+
+	@Test
+	public void add_long() {
+		RationalNumber r;
+
+		r = new RationalNumber(1, 1).add(1L);
+		assertThat(r.numerator(), is(2L));
+		assertThat(r.denominator(), is(1L));
+		assertThat(r.isNegative(), is(false));
+
+		r = new RationalNumber(1, 2).add(1L);
+		assertThat(r.numerator(), is(3L));
+		assertThat(r.denominator(), is(2L));
+		assertThat(r.isNegative(), is(false));
+
+		r = new RationalNumber(5, 3).add(2L);
+		assertThat(r.numerator(), is(11L));
+		assertThat(r.denominator(), is(3L));
+		assertThat(r.isNegative(), is(false));
+
+		r = new RationalNumber(5, 3).add(-2L);
+		assertThat(r.numerator(), is(1L));
+		assertThat(r.denominator(), is(3L));
+		assertThat(r.isNegative(), is(true));
+
+		r = new RationalNumber(-5, 3).add(-2L);
+		assertThat(r.numerator(), is(11L));
+		assertThat(r.denominator(), is(3L));
+		assertThat(r.isNegative(), is(true));
+
+		r = new RationalNumber(-5, 3).add(2L);
+		assertThat(r.numerator(), is(1L));
+		assertThat(r.denominator(), is(3L));
+		assertThat(r.isNegative(), is(false));
+	}
+
+	@Test
+	public void add_RationalNumber() {
+		RationalNumber r;
+
+		r = new RationalNumber(1, 1).add(new RationalNumber(1, 1));
+		assertThat(r.numerator(), is(2L));
+		assertThat(r.denominator(), is(1L));
+		assertThat(r.isNegative(), is(false));
+
+		r = new RationalNumber(1, 1).add(new RationalNumber(1, 2));
+		assertThat(r.numerator(), is(3L));
+		assertThat(r.denominator(), is(2L));
+		assertThat(r.isNegative(), is(false));
+
+		r = new RationalNumber(1, 2).add(new RationalNumber(1, 2));
+		assertThat(r.numerator(), is(1L));
+		assertThat(r.denominator(), is(1L));
+		assertThat(r.isNegative(), is(false));
+
+		r = new RationalNumber(1, 2).add(new RationalNumber(1, 3));
+		assertThat(r.numerator(), is(5L));
+		assertThat(r.denominator(), is(6L));
+		assertThat(r.isNegative(), is(false));
+
+		r = new RationalNumber(2, 1).add(new RationalNumber(-1, 3));
+		assertThat(r.numerator(), is(5L));
+		assertThat(r.denominator(), is(3L));
+		assertThat(r.isNegative(), is(false));
+
+		r = new RationalNumber(-3, 1).add(new RationalNumber(1, 4));
+		assertThat(r.numerator(), is(11L));
+		assertThat(r.denominator(), is(4L));
+		assertThat(r.isNegative(), is(true));
+
+		r = new RationalNumber(-2, 3).add(new RationalNumber(-3, 4));
+		assertThat(r.numerator(), is(17L));
+		assertThat(r.denominator(), is(12L));
+		assertThat(r.isNegative(), is(true));
+	}
+
+	@Test
+	public void reciprocal_works() {
+		RationalNumber r;
+
+		r = new RationalNumber(1, 2).reciprocal();
+		assertThat(r.numerator(), is(2L));
+		assertThat(r.denominator(), is(1L));
+		assertThat(r.isNegative(), is(false));
+
+		r = new RationalNumber(3, -17).reciprocal();
+		assertThat(r.numerator(), is(17L));
+		assertThat(r.denominator(), is(3L));
+		assertThat(r.isNegative(), is(true));
+
+		r = new RationalNumber(4, 6).reciprocal();
+		assertThat(r.numerator(), is(3L));
+		assertThat(r.denominator(), is(2L));
+		assertThat(r.isNegative(), is(false));
+	}
+
+	@Test
+	public void intValue_works() {
+		assertThat(new RationalNumber(0, 1).intValue(), is(0));
+		assertThat(new RationalNumber(1, 1).intValue(), is(1));
+		assertThat(new RationalNumber(-1, 1).intValue(), is(-1));
+		assertThat(new RationalNumber(1, -1).intValue(), is(-1));
+		assertThat(new RationalNumber(-1, -1).intValue(), is(1));
+		assertThat(new RationalNumber(1, 2).intValue(), is(0));
+		assertThat(new RationalNumber(3, 4).intValue(), is(0));
+		assertThat(new RationalNumber(4, 4).intValue(), is(1));
+		assertThat(new RationalNumber(5, 4).intValue(), is(1));
+		assertThat(new RationalNumber(7, 4).intValue(), is(1));
+		assertThat(new RationalNumber(8, 4).intValue(), is(2));
+		assertThat(new RationalNumber(Long.MAX_VALUE, 1).intValue(), is(-1));
+	}
+	
+	@Test
+	public void longValue_works() {
+		assertThat(new RationalNumber(0, 1).longValue(), is(0L));
+		assertThat(new RationalNumber(1, 1).longValue(), is(1L));
+		assertThat(new RationalNumber(-1, 1).longValue(), is(-1L));
+		assertThat(new RationalNumber(1, -1).longValue(), is(-1L));
+		assertThat(new RationalNumber(-1, -1).longValue(), is(1L));
+		assertThat(new RationalNumber(1, 2).longValue(), is(0L));
+		assertThat(new RationalNumber(3, 4).longValue(), is(0L));
+		assertThat(new RationalNumber(4, 4).longValue(), is(1L));
+		assertThat(new RationalNumber(5, 4).longValue(), is(1L));
+		assertThat(new RationalNumber(7, 4).longValue(), is(1L));
+		assertThat(new RationalNumber(8, 4).longValue(), is(2L));
+		assertThat(new RationalNumber(Long.MAX_VALUE, 1).longValue(), is(Long.MAX_VALUE));
+	}
+	
+	@Test
+	public void floatValue_works() {
+		assertThat(new RationalNumber(0, 1).floatValue(), is(0f));
+		assertThat(new RationalNumber(1, 1).floatValue(), is(1f));
+		assertThat(new RationalNumber(-1, 1).floatValue(), is(-1f));
+		assertThat(new RationalNumber(1, -1).floatValue(), is(-1f));
+		assertThat(new RationalNumber(-1, -1).floatValue(), is(1f));
+		assertThat(new RationalNumber(1, 2).floatValue(), is(0.5f));
+		assertThat(new RationalNumber(3, 4).floatValue(), is(0.75f));
+		assertThat(new RationalNumber(4, 4).floatValue(), is(1f));
+		assertThat(new RationalNumber(5, 4).floatValue(), is(1.25f));
+		assertThat(new RationalNumber(7, 4).floatValue(), is(1.75f));
+		assertThat(new RationalNumber(8, 4).floatValue(), is(2f));
+		assertThat(new RationalNumber(Long.MAX_VALUE, 1).floatValue(), is((float) Long.MAX_VALUE));
+	}
+	
+	@Test
+	public void doubleValue_works() {
+		assertThat(new RationalNumber(0, 1).doubleValue(), is(0.0));
+		assertThat(new RationalNumber(1, 1).doubleValue(), is(1.0));
+		assertThat(new RationalNumber(-1, 1).doubleValue(), is(-1.0));
+		assertThat(new RationalNumber(1, -1).doubleValue(), is(-1.0));
+		assertThat(new RationalNumber(-1, -1).doubleValue(), is(1.0));
+		assertThat(new RationalNumber(1, 2).doubleValue(), is(0.5));
+		assertThat(new RationalNumber(3, 4).doubleValue(), is(0.75));
+		assertThat(new RationalNumber(4, 4).doubleValue(), is(1.0));
+		assertThat(new RationalNumber(5, 4).doubleValue(), is(1.25));
+		assertThat(new RationalNumber(7, 4).doubleValue(), is(1.75));
+		assertThat(new RationalNumber(8, 4).doubleValue(), is(2.0));
+		assertThat(new RationalNumber(Long.MAX_VALUE, 1).doubleValue(), is((double) Long.MAX_VALUE));
+	}
+
+	@Test
+	public void isInteger_works() {
+		assertThat(new RationalNumber(0, 1).isInteger(), is(true));
+		assertThat(new RationalNumber(1, 1).isInteger(), is(true));
+		assertThat(new RationalNumber(1, 2).isInteger(), is(false));
+		assertThat(new RationalNumber(3, 4).isInteger(), is(false));
+		assertThat(new RationalNumber(4, 4).isInteger(), is(true));
+		assertThat(new RationalNumber(5, 4).isInteger(), is(false));
+		assertThat(new RationalNumber(7, 4).isInteger(), is(false));
+		assertThat(new RationalNumber(8, 4).isInteger(), is(true));
+		assertThat(new RationalNumber(Long.MAX_VALUE, 1).isInteger(), is(true));
+	}
+
+	@Test
+	public void isNegative_works() {
+		assertThat(new RationalNumber(0, 1).isNegative(), is(false));
+		assertThat(new RationalNumber(0, -1).isNegative(), is(false));
+		assertThat(new RationalNumber(-1, -1).isNegative(), is(false));
+		assertThat(new RationalNumber(1, -1).isNegative(), is(true));
+		assertThat(new RationalNumber(-1, 1).isNegative(), is(true));
+		assertThat(new RationalNumber(1, 1).isNegative(), is(false));
+	}
+
+	@Test
+	public void signum_works() {
+		assertThat(new RationalNumber(0, 1).signum(), is(1L));
+		assertThat(new RationalNumber(0, -1).signum(), is(1L));
+		assertThat(new RationalNumber(1, 2).signum(), is(1L));
+		assertThat(new RationalNumber(1, -2).signum(), is(-1L));
+		assertThat(new RationalNumber(-1, 2).signum(), is(-1L));
+		assertThat(new RationalNumber(-1, -2).signum(), is(1L));
+	}
+}
